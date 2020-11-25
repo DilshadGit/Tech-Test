513b7599c8bf073414d4094c7922314fb677db82
DilshadGit <dilshad.abdulla@icloud.com>
2020-11-25 23:25:57 +0000

    Search for unique elements in the list and add them together, the other are repeated ingore it.

--- tasks/exercise001.py
+++ tasks/exercise001.py
@@ -6,20 +6,35 @@
 # More examples in the test cases below.
 
 # Good luck!
-
-# arr = [4, 5, 7, 5, 4, 8] # result 15
-# arr = [9, 10, 19, 13, 19, 13] # result 19
-# arr = [16, 0, 11, 4, 8, 16, 0, 11] # result 12
-# arr = [5, 17, 18, 11, 13, 18, 11, 13] # result 22
-arr = [5, 10, 19, 13, 10, 13] # result 24
+'''
+assert repeats([4,5,7,5,4,8]) == 15
+assert repeats([9, 10, 19, 13, 19, 13]) == 19
+assert repeats([16, 0, 11, 4, 8, 16, 0, 11]) == 12
+assert repeats([5, 17, 18, 11, 13, 18, 11, 13]) == 22
+assert repeats([5, 10, 19, 13, 10, 13]) == 24
+'''
 
 def repeats(arr):
+    test = int(input('Enter number between 1-5: '))
+    if test == 1:
+        arr = [4,5,7,5,4,8]
+    elif test == 2:
+        arr = [9, 10, 19, 13, 19, 13]
+    elif test == 3:
+        arr = [16, 0, 11, 4, 8, 16, 0, 11]
+    elif test == 4:
+        arr = [5, 17, 18, 11, 13, 18, 11, 13]
+    elif test == 5:
+        arr = [5, 10, 19, 13, 10, 13]
+    else:
+        print('You entered out of range')
+
     total = 0
 
     for num in arr:
         if arr.count(num) == 1:
             total += num
 
-    return total
+    return f'{arr} == {total}'
 
-print(repeats(arr))
+print(repeats(arr=input('Press enter')))
